e21936c20c3a4e4c0389841dd116e009e543b94e
Duplessier Bastien <bastien.duplessier@gmail.com>
2014-11-21 09:15:11 +0100

    Add movement

--- Assets/Scripts/Moto/Movement.cs
+++ Assets/Scripts/Moto/Movement.cs
@@ -2,9 +2,28 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-	
+
+	private float velocity = 2;
+	private Vector3 translation = Vector3.zero;
+
 	// Update is called once per frame
 	void Update () {
-	
+		UpdateMovement ();
+	}
+
+	private void UpdateMovement() {
+		UpdateVelocity ();
+		UpdateTranslation ();
+
+
+		gameObject.transform.Translate (translation);
+	}
+	private void UpdateTranslation() {
+		translation.x = velocity;
+	}
+
+
+	private void UpdateVelocity() {
+		return;
 	}
 }
