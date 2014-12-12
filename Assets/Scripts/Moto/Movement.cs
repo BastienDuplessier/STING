77f96a67141127c5f59f14343eccdb3ef0e6f0ab
Duplessier Bastien <bastien.duplessier@gmail.com>
2014-12-12 23:22:57 +0100

    Improve speed calculation

--- Assets/Scripts/Moto/Movement.cs
+++ Assets/Scripts/Moto/Movement.cs
@@ -4,12 +4,22 @@ using AssemblyCSharp;
 
 public class Movement : MonoBehaviour {
 
-	public float speed = 15F;
+	// Acceleration factors
+	protected float minSpeed = 15F;
+	protected float maxSpeed = 100F;
+	protected float lifting = 0.1F; // Smooth the acceleration. 0 > lifting > 1
+	protected float const_a; // Needed, don't touch.
+	protected int frame_count;
+
+	public float speed;
 	protected Control controller;
 
 	public virtual void Start() {
 		DefineControlMethod ();
 		this.gameObject.AddComponent("SmoothTrailRendererCollider");
+		this.frame_count = 0;
+		this.speed = this.minSpeed;
+    	this.const_a = maxSpeed - minSpeed;
 	}
 
 	public virtual void Update () {
@@ -19,22 +29,31 @@ public class Movement : MonoBehaviour {
 	}
 
 	public void UpdateMovement (){
-		speed = 15F;
 		if (controller.TurningRight ()) {
 			transform.Rotate (Vector3.up * (speed * 0.1F));
 		} 
 		if (controller.TurningLeft ()) {
 			transform.Rotate (Vector3.down * (speed * 0.1F));
 		}
-		if(controller.Accelerating())
-			speed = 20F;
-		if(controller.Slowing())
-			speed = 10F;
+		if (controller.Accelerating ()) {
+			this.speed = acceleration (frame_count);
+			frame_count += 1;
+		} else {
+			frame_count = 0;
+			if(controller.Slowing())
+				this.speed = Mathf.Max(this.speed - 1F, this.minSpeed);
+			else
+				this.speed = Mathf.Max(this.speed - 0.5F, this.minSpeed);
+		}
 
 	}
 	
 	public virtual void DefineControlMethod() {
 		controller = new Keyboard (this);
+	}
+
+	private float acceleration(int x) {
+		return (-this.const_a) * Mathf.Exp (-this.lifting * x) + this.maxSpeed;
 	}
 
 }
