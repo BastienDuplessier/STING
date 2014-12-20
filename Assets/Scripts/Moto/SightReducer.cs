6a426efa9de86c962f984542bcc9dfac62a3cc64
Duplessier Bastien <bastien.duplessier@gmail.com>
2014-12-20 21:00:29 +0100

    Add script to reduce sight

---
+++ Assets/Scripts/Moto/SightReducer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SightReducer : MonoBehaviour {
+
+	private RectTransform rTransform;
+	private float baseScaleX = 4F;
+	private float minScaleX = 0.5F;
+	private float rangeX;
+	
+	// Use this for initialization
+	void Start () {
+		this.rTransform = gameObject.GetComponent<RectTransform> ();
+		rangeX = Mathf.Abs(baseScaleX - minScaleX);
+	}
+	
+	public void UpdateHiding(float rate) {
+		float newScaleX = this.baseScaleX - (rate * this.rangeX);
+		rTransform.localScale = new Vector3 (newScaleX, 1F, 1F);
+	}
+}
