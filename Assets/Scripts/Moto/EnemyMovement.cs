4e4ba0bf8d4d5bd3c4a373ff6360a036a8cc0f22
Duplessier Bastien <bastien.duplessier@gmail.com>
2015-01-08 21:36:07 +0100

    Do better IA

--- Assets/Scripts/Moto/EnemyMovement.cs
+++ Assets/Scripts/Moto/EnemyMovement.cs
@@ -4,8 +4,15 @@ using AssemblyCSharp;
 
 public class EnemyMovement : Movement
 {
+	protected new float maxSpeed = 30;
+
 	public override void DefineControlMethod() {
-		this.controller = new IAControl (this, gameObject);
+		this.controller = new IAControl (this, gameObject, this.maxSpeed);
+	}
+
+	public override void Update() {
+		base.Update ();
+		this.controller.Update ();
 	}
 }
 
